2b89435158dcf71baf466a950fd4790796b47fcd
Jerker <jerker_a@msn.com>
2016-05-14 12:36:48 +0200

    Fixat default startup-URL.

--- Laurene.WebApp/app/app.module.js
+++ Laurene.WebApp/app/app.module.js
@@ -1,16 +1,16 @@
 ﻿(function () {
 
-    var app = angular.module('laureneApp', ['ngRoute'])
+    var app = angular.module('app', ['ngRoute'])
         .config(function ($routeProvider, $locationProvider) {
             $routeProvider.when('/', {
-                templateUrl: 'index.html',
+                templateUrl: 'app/index.html',
                 controller: 'MainMenuController'
             });
             $routeProvider.when('/2', {
                 templateUrl: 'templates/Beers.html',
                 controller: 'BeersController'
             });
-            $routeProvider.otherwise({ redirectTo: '/' });
+            $routeProvider.otherwise({ redirectTo: 'app/' });
             $locationProvider.html5Mode(true);
         });
 })();
